1a12907fd06ea76f78745f3009b45f3da77a9997
Lurgen <76846561+Protogen-Lurgen@users.noreply.github.com>
2021-11-17 14:12:00 -0300

    Adição de aplicações desenvolvidas anteriormente
    Então pessoal, coloquei dois apps em Python que desenvolvi para o grupo que estou participando do PJI do IFSP-PTB de 2º Ano de Redes.
    
    Ambos os arquivos são do back-end do website que está sendo desenvolvido, onde foram programados de forma bem simples com base em bastante pesquisa.

---
+++ Aplicações desenvolvidas anteriormente/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def show_index():
+  return render_template('index.html')
+
+
+def api_response():
+  from flask import jsonify
+  if request.method == 'POST':
+    return jsonify(**request.json)
+
+
+@app.route('/dados', methods=['POST'])
+def get_data_from_html():
+  Emails11 = request.form['EMAIL']
+  Emails12 = open("/Users/lukin/Desktop/Site - MEO/emails.txt", "a")
+  print("O e-mail cadastrado foi: " + Emails11)
+  Emails12.write(" " + Emails11 + ",")
+
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run('localhost', 8000)
+
+# Aplicação para armazenar dados de input de email de um website num arquivo de texto.
+
+# Versão do Python utilizada para o desenvolvimento da aplicação: Python 3.9
---
+++ Aplicações desenvolvidas anteriormente/sendgmail.py
@@ -0,0 +1,61 @@
+﻿# -*- coding: utf-8 -*-
+# =============================================================================
+# Imports
+# =============================================================================
+import smtplib
+
+ListaEmails = open('/Users/lukin/Desktop/Site - MEO/emails.txt')
+emails = ListaEmails.read()
+print(emails)
+for item in emails.split():
+  ListaEmails2 = emails[0:].split()
+print("\n\n\n\n")
+# =============================================================================
+# Informações de Login
+# =============================================================================
+gmail_user = '(email)'
+gmail_app_password = '(senha)'
+# =============================================================================
+# Informações do E-Mail
+# =============================================================================
+sent_from = gmail_user
+sent_to = ListaEmails2
+sent_subject = "Email do Furry"  # Título do e-mail
+sent_body = (u"Gente, finalmente, gra\u00e7as a Deus.\n\n"
+             u"Depois de muita a\u00e7\u00e3o\n"
+             # https://unicode-table.com/pt/ | Palavras em Unicode, que estão fora da tabela ASCII.
+             u"\n"
+             u"a API está funcionando corretamente.\n\n"
+             u"Atenciosamente,\n"
+             u"Lurgen, conhecido como Lucas.\n")  # Corpo do e-mail
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+
+email_text.encode('utf-8')
+
+print(email_text)
+# =============================================================================
+# SEND EMAIL OR DIE TRYING!!! | MANDE EMAIL OU MORRA TENTANDO!!!
+# Detalhes: http://www.samlogic.net/articles/smtp-commands-reference.htm
+# =============================================================================
+
+try:
+  server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+  server.ehlo()
+  server.login(gmail_user, gmail_app_password)
+  server.sendmail(sent_from, sent_to, email_text.encode('utf-8'))
+  server.close()
+
+  print('Email enviado!')
+except Exception as exception:
+  print("Erro: %s!\n\n" % exception)
+
+# Aplicação montada para o envio múltiplo e simultâneo de e-mails cadastrados no arquivo de texto de acordo com a outra aplicação montada em Flask.
+
+# Versão do Python utilizada para o desenvolvimento da aplicação: Python 3.9
